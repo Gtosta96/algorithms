64ce589cb9d1a6c31e02ec41ad2e19c50ad64938
Gtosta96 <gabrieltosta3@gmail.com>
2019-01-18 00:40:25 -0200

    add stack in data-structures folder

---
+++ data-structures/stack.js
@@ -0,0 +1,37 @@
+// FILO
+
+class Stack {
+  constructor() {
+    this.data = [];
+    this.top = -1;
+  }
+
+  push(value) {
+    this.top++;
+    this.data[this.top] = value;
+  }
+
+  pop() {
+    const item = this.data[this.top];
+    this.data.splice(this.top, 1);
+    this.top--;
+
+    return item;
+  }
+
+  print() {
+    console.log(' ');
+    console.log('-----');
+    console.log(this.data);
+    console.log('-----');
+    console.log(' ');
+  }
+}
+
+const stack = new Stack();
+stack.push(1)
+stack.push(2)
+
+stack.pop();
+
+stack.print();
